62a993a35ac943696ac57f7404a838998ec7d6f2
Karolpz <carolinelpz34@gmail.com>
2024-12-19 15:14:46 +0100

    update

---
+++ p7-kasa/src/pages/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div>
+        <h1>404</h1>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <p>Retourner sur la page d’accueil</p>
+    </div>
+  )
+}
+
+export default ErrorPage
