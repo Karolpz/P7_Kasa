9e07b5d6a0bce300ce73c8fd47781ac9dd03f6a7
Karolpz <carolinelpz34@gmail.com>
2024-12-31 15:24:46 +0100

    cards

--- p7-kasa/src/components/Card.jsx
+++ p7-kasa/src/components/Card.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import logements from '../logements.json'
 
 const Card = () => {
   return (
-    <div>
-        
+    <div className='cardsContainer'>
+
+            {logements.map((logement) => (
+                <div key = {logement.id} className='cardContent'>
+                    <img src = {logement.cover} alt = {logement.title} className='cardPicture'/>
+                    <h2>{logement.title}</h2>
+                </div>
+            ))}
+
     </div>
   )
 }
--- p7-kasa/src/pages/Home.jsx
+++ p7-kasa/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Banner from '../components/Banner'
+import Card from '../components/Card'
 
 
 const Home = () => {
   return (
     <div>
         <Banner />
+        <Card />
     </div>
   )
 }
